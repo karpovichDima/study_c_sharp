e9c2c4ecbfe5dc7c014c1e7f1309a36da2d9b2fb
Dmitry Karpovich <d.karpovich@godeltech.com>
2018-11-26 10:34:49 +0300

    Add project files.

---
+++ LambdaAndLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaAndLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
